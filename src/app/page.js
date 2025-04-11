d080b973c96699cded0eff80b759197a4fcab179
Manoel Mecias <mmnc12@gmail.com>
2025-04-11 13:44:04 -0300

    barbearia projeto incompleto

--- src/app/page.js
+++ src/app/page.js
@@ -31,23 +31,25 @@ export default function Home() {
 
         {/* Inicio contato */}
         <section className={estilos.end_contato}>
-          <div className={estilos.endereco}>
-            <FaLocationDot className={estilos.icon} />
-            <h2>ENDEREÇO</h2>
-            <div className={estilos.linha}></div>
-            <p>TRAV. REGIS PACHECO, SN, AO LADO DA PASTELARIA DAYSE ANNE</p>
-          </div>
-          <div className={estilos.zap}>
-            <BiSolidPhoneCall className={estilos.icon}/>
-            <h2>WHATSAPP</h2>
-            <div className={estilos.linha}></div>
-            <p>(74) 99999-9999</p>
-          </div>
-          <div className={estilos.atendimento}>
-            <FaClock className={estilos.icon}/>
-            <h2>ATENDIMENTO</h2>
-            <div className={estilos.linha}></div>
-            <p>SEG À SÁB: 8:00 ÁS 19:00</p>
+          <div className={estilos.container_endereco}>
+            <div className={estilos.endereco}>
+              <FaLocationDot className={estilos.icon} />
+              <h2>ENDEREÇO</h2>
+              <div className={estilos.linha}></div>
+              <p>TRAV. REGIS PACHECO, SN, AO LADO DA PASTELARIA DAYSE ANNE</p>
+            </div>
+            <div className={estilos.zap}>
+              <BiSolidPhoneCall className={estilos.icon} />
+              <h2>WHATSAPP</h2>
+              <div className={estilos.linha}></div>
+              <p>(74) 99999-9999</p>
+            </div>
+            <div className={estilos.atendimento}>
+              <FaClock className={estilos.icon} />
+              <h2>ATENDIMENTO</h2>
+              <div className={estilos.linha}></div>
+              <p>SEG À SÁB: 8:00 ÁS 19:00</p>
+            </div>
           </div>
         </section>
         {/* Fim contato */}
