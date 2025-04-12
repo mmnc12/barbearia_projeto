f61bd4841ae99d31e518d52c81193b8a0d518f1b
Manoel Mecias <mmnc12@gmail.com>
2025-04-12 01:05:45 -0300

    projeto_barbearia

--- src/app/page.js
+++ src/app/page.js
@@ -31,29 +31,38 @@ export default function Home() {
 
         {/* Inicio contato */}
         <section className={estilos.end_contato}>
-          <div className={estilos.container_endereco}>
-            <div className={estilos.endereco}>
-              <FaLocationDot className={estilos.icon} />
-              <h2>ENDEREÇO</h2>
-              <div className={estilos.linha}></div>
-              <p>TRAV. REGIS PACHECO, SN, AO LADO DA PASTELARIA DAYSE ANNE</p>
-            </div>
-            <div className={estilos.zap}>
-              <BiSolidPhoneCall className={estilos.icon} />
-              <h2>WHATSAPP</h2>
-              <div className={estilos.linha}></div>
-              <p>(74) 99999-9999</p>
-            </div>
-            <div className={estilos.atendimento}>
-              <FaClock className={estilos.icon} />
-              <h2>ATENDIMENTO</h2>
-              <div className={estilos.linha}></div>
-              <p>SEG À SÁB: 8:00 ÁS 19:00</p>
-            </div>
+          <div className={estilos.endereco}>
+            <FaLocationDot className={estilos.icon} />
+            <h2>ENDEREÇO</h2>
+            <div className={estilos.linha}></div>
+            <p>TRAV. REGIS PACHECO, SN, AO LADO DA PASTELARIA DAYSE ANNE</p>
+          </div>
+          <div className={estilos.zap}>
+            <BiSolidPhoneCall className={estilos.icon} />
+            <h2>WHATSAPP</h2>
+            <div className={estilos.linha}></div>
+            <p>(74) 99999-9999</p>
+          </div>
+          <div className={estilos.atendimento}>
+            <FaClock className={estilos.icon} />
+            <h2>ATENDIMENTO</h2>
+            <div className={estilos.linha}></div>
+            <p>SEG À SÁB: 8:00 ÁS 19:00</p>
           </div>
         </section>
         {/* Fim contato */}
-
+        
+        <section className={estilos.qualificacao}>
+          <article>
+            <h2>A melhor barbearia da região, com estilo e conforto bem perto de você!</h2>
+            <p>
+              A Barbearia do João está localizada no centro da cidade, de fácil acesso. Oferecemos cortes de cabelo para adultos e crianças, e também cuidamos da sua barba com todo o carinho e atenção.
+            </p>
+          </article>
+          <aside>
+            <img src="img21cab.png" alt="imagem de corte de cabelo" />
+          </aside>
+        </section>
       </main>
     </div>
   );
