012e30829b5fee74cb30c3b889d6bdbc75f230b6
Manoel Mecias <mmnc12@gmail.com>
2025-04-12 22:33:39 -0300

    finalizando a parte mobile

--- src/app/page.js
+++ src/app/page.js
@@ -322,10 +322,13 @@ export default function Home() {
         </section>
         {/* Fim da seção agendamento */}
 
+        {/* Início seção localização */}
         <section className={estilos.localizacao}>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d310.06869118632443!2d-40.36307300539684!3d-10.74367402025921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x76d1437c8855aff%3A0x5e96352a16b17889!2sPindoba%C3%A7u%2C%20BA%2C%2044770-000!5e1!3m2!1spt-BR!2sbr!4v1740946154404!5m2!1spt-BR!2sbr" width="100%" height="450" className="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </section>
+        {/* Fim seção localização */}
 
+        {/* Início seção footer */}
         <footer className={estilos.footer}>
           <img src="banner2.png" alt="logo" />
           <nav>
@@ -344,9 +347,13 @@ export default function Home() {
             <p>&reg; 2025 - Desenvolvido por "Manoel Mecias do Nascimento"</p>
           </div>
         </footer>
+        {/* Fim seção footer */}
+
+        {/* Inicio do botão whatsapp */}
         <a href="https://wa.me/99999999999" className={estilos.whatsappButton} target="_blank" rel="noopener noreferrer">
           <FaWhatsapp className={estilos.whatsappIcon} />
         </a>
+        {/* Fim do botão whatsapp */}
       </main>
     </div>
   );
